384e1dddee3d7712c1e7debf67a62a689ba034ba
Alejandramelo2018-create <emilianomelo2018@gmail.com>
2026-02-27 10:15:59 -0500

    Se crea el codigo base

---
+++ src/main/java/com/nodo/retotecnico/RetoTecnicoApplication.java
@@ -0,0 +1,13 @@
+package com.nodo.retotecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class gitRetoTecnicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetoTecnicoApplication.class, args);
+    }
+
+}
