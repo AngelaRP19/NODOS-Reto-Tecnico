7ece8a7e5a9179e9ff533481263b534210620e0d
Angela Peña <131418367+AngelaRP19@users.noreply.github.com>
2026-03-10 06:31:38 -0500

    Fix generic type declaration in BuysRepository

--- src/main/java/com/nodo/retotecnico/repository/BuysRepository.java
+++ src/main/java/com/nodo/retotecnico/repository/BuysRepository.java
@@ -3,6 +3,6 @@ package com.nodo.retotecnico.repository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface  BuysRepository extends JpaRepository<Buy Integer>{
+public interface  BuysRepository extends JpaRepository<Buy, Integer>{
     
 }
