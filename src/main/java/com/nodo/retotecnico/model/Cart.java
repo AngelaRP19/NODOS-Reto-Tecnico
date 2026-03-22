5eec9434065f87f62176e50c2c29346364658e8d
majosanm8 <majosanm8@gmail.com>
2026-03-22 14:52:42 -0500

    Crear la clase - entidad para el carrito de compras

---
+++ src/main/java/com/nodo/retotecnico/model/Cart.java
@@ -0,0 +1,61 @@
+package com.nodo.retotecnico.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "cart")
+@Getter
+@Setter
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    // Relación con usuario
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    // Relación con expansiones
+    @ManyToMany
+    @JoinTable(
+        name = "cart_expansion",
+        joinColumns = @JoinColumn(name = "cart_id"),
+        inverseJoinColumns = @JoinColumn(name = "expansion_id")
+    )
+    private List<ExpansionPack> expansions = new ArrayList<>();
+
+    public Cart() {}
+
+    public Cart(User user) {
+        this.user = user;
+    }
+
+    // MÉTODOS
+
+    public void addExpansion(ExpansionPack expansion) {
+        if (!this.expansions.contains(expansion)) {
+            this.expansions.add(expansion);
+        }
+    }
+
+    public void removeExpansion(ExpansionPack expansion) {
+        this.expansions.remove(expansion);
+    }
+
+    public void clearCart() {
+        this.expansions.clear();
+    }
+
+    public double getTotal() {
+        return expansions.stream()
+                .mapToDouble(ExpansionPack::getPrice)
+                .sum();
+    }
+}
