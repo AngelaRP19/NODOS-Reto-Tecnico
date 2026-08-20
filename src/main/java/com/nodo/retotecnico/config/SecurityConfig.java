62e2206d2994a91b2d34e2f6b04d6fab36808fd9
Angela Peña <131418367+AngelaRP19@users.noreply.github.com>
2026-08-20 15:46:00 -0500

    Annotate oAuth2AuthenticationSuccessHandler with @Lazy
    Add @Lazy annotation to oAuth2AuthenticationSuccessHandler for lazy initialization.

--- src/main/java/com/nodo/retotecnico/config/SecurityConfig.java
+++ src/main/java/com/nodo/retotecnico/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -38,6 +39,7 @@ public class SecurityConfig {
     }
 
     @Autowired
+    @Lazy
     private OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
 
 
